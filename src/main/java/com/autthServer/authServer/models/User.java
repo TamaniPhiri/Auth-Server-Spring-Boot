7d63ef16e4985d6601f7d0fd4918d01e7488e6eb
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 13:35:14 +0200

    changes

---
+++ src/main/java/com/autthServer/authServer/models/User.java
@@ -0,0 +1,31 @@
+package com.autthServer.authServer.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column
+    private String password
+
+}
