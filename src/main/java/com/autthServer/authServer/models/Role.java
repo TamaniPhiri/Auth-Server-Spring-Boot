e9094efe481c667358759c35c552cbeecb13967c
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 14:14:22 +0200

    changes

---
+++ src/main/java/com/autthServer/authServer/models/Role.java
@@ -0,0 +1,18 @@
+package com.autthServer.authServer.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+}
