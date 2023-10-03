bdcd84d308e9c3c8fa48eb6c051bb4c3dd944814
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 13:39:43 +0200

    changes

--- src/main/java/com/autthServer/authServer/models/User.java
+++ src/main/java/com/autthServer/authServer/models/User.java
@@ -32,6 +32,8 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"))
+    @JoinTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id")
+    )
     private Set<Role> roles;
 }
