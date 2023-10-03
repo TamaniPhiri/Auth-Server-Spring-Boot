19fe19659b157008bef4a24305cd54a2c4699bee
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 15:22:09 +0200

    changes

--- src/main/java/com/autthServer/authServer/JwtTokenProvider.java
+++ src/main/java/com/autthServer/authServer/JwtTokenProvider.java
@@ -1,7 +1,16 @@
 package com.autthServer.authServer;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
 
 @Component
 public class JwtTokenProvider {
+    private static final Logger logger= LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
 }
