60c777838e7fab77a5a432751cffa0abce05f3aa
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 14:16:16 +0200

    changes

--- src/main/java/com/autthServer/authServer/models/Role.java
+++ src/main/java/com/autthServer/authServer/models/Role.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
