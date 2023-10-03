b6804bb7cbc37e08e2e6766cacd4b30117daf7d0
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 14:37:03 +0200

    changes

--- src/main/java/com/autthServer/authServer/repositories/RoleRepository.java
+++ src/main/java/com/autthServer/authServer/repositories/RoleRepository.java
@@ -2,7 +2,6 @@ package com.autthServer.authServer.repositories;
 
 import com.autthServer.authServer.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
