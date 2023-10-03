e515f7be520792de4387672fa467852de54c4b95
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 14:37:14 +0200

    changes

--- src/main/java/com/autthServer/authServer/repositories/UserRepository.java
+++ src/main/java/com/autthServer/authServer/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.autthServer.authServer.repositories;
 
 import com.autthServer.authServer.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
