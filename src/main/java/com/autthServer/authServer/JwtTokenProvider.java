968180422d13cc46937993fac2b00097a3eed57b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 17:42:40 +0200

    changes

--- src/main/java/com/autthServer/authServer/JwtTokenProvider.java
+++ src/main/java/com/autthServer/authServer/JwtTokenProvider.java
@@ -1,9 +1,15 @@
 package com.autthServer.authServer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Value;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
@@ -17,6 +23,24 @@ public class JwtTokenProvider {
 
 //    generate JWT token
     public String generateToken(Authentication authentication){
-        String username=authentication
+        String username=authentication.getName();
+
+        Date currentDate = new Date();
+
+        Date expiredate=new Date(currentDate.getTime()+jwtExpirationDate);
+
+        String token= Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredate)
+                .signWith(key())
+                .compact();
+        return token;
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
     }
 }
