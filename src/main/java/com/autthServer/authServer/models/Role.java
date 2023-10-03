a5a84b124a289dae21e421d5fc09ba3c9019fb94
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 14:16:01 +0200

    changes

--- src/main/java/com/autthServer/authServer/models/Role.java
+++ src/main/java/com/autthServer/authServer/models/Role.java
@@ -1,9 +1,6 @@
 package com.autthServer.authServer.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,5 +14,7 @@ import lombok.Setter;
 @Table(name = "role")
 public class Role {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private  String name;
 }
